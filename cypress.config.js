5238d5b1592f47029f215fdea3cf6c6d93aeca87
Jennie Evans <jennie1evans@hotmail.com>
2022-07-17 22:40:52 +0100

    set up updates and first integration test passing

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3030'
+    baseUrl: 'http://localhost:3000',
+    supportFile: false
   }
 })
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render('index', { title: "Gingergram"})
+});
 
 module.exports = router
